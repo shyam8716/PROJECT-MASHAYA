76bac8b056f6145eddf2a621e624058a67a2a902
shyam <shyammegha493@gmail.com>
2025-08-20 16:54:31 +0530

    added ARTHEMATIC OPERATION.java

---
+++ ARTHEMATIC OPERATION.java
@@ -0,0 +1,12 @@
+import java.io.*;
+class Arthematic_operations
+{
+    public static void main(String args[])
+    {
+        int x=5;
+        int y=55;
+        int sum;
+        sum=x+y;
+        System.out.print("sum is:"+sum);
+    }
+}
