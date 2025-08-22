20744d993a2cabf7d917c2b02cf19edbf1ec56e9
shyam <shyammegha493@gmail.com>
2025-08-22 17:48:56 +0530

    Added AREA OF TRIANGLE.java program

---
+++ AREA OF TRIANGLE.java
@@ -0,0 +1,12 @@
+import java.io.*;
+class Triangle
+{
+    public static void main(String args[])
+    {
+        double b = 10;   
+        double h = 5;
+        double area;
+        area=0.5*b*h;
+        System.out.print("area of triangle"+area);
+    }
+}
