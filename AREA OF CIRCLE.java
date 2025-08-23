702fa5d747ad1ca8fa3e6fc2434405d3e337c0ce
shyam <shyammegha493@gmail.com>
2025-08-23 17:05:28 +0530

    added AREA OF CIRCLE.java

---
+++ AREA OF CIRCLE.java
@@ -0,0 +1,11 @@
+import java.io.*;
+class Circle
+{
+    public static void main(String args[])
+    {
+        
+        double r=2, area;
+        area=3.14*r*r;
+        System.out.print("area of circle"+area);
+    }
+}
