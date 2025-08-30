229f1875b463d4eb9abcbc48851c9fddc7e420ce
shyam <shyammegha493@gmail.com>
2025-08-30 11:22:41 +0530

    added EMPLOYEE DETAILS.java

---
+++ EMPLOYEE DETAILS.java
@@ -0,0 +1,12 @@
+import java.io.*;
+class Employee{
+public static void main(String edc[])
+{
+    String ename="rahul";
+    int eid=1030;
+    float esal=45000;
+   System.out.println("Employee Name: " + ename);
+   System.out.println("Employee ID: " + eid);
+   System.out.println("Employee Salary: " + esal);
+}
+}
