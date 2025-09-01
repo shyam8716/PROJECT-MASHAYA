b65cfabe634ccaa8ce082cf44cb275e9b0991384
shyam <shyammegha493@gmail.com>
2025-09-01 11:52:39 +0530

    added 3 SUBJECTS.java

---
+++ 3 SUBJECTS.java
@@ -0,0 +1,15 @@
+import java.io.*;
+class Stu {
+    public static void main(String edc[]) {
+        int maths = 5, statistics = 10, computers = 15, total;
+        double average;
+        total = maths + statistics + computers;
+        average = total / 3.0;
+        System.out.println("Maths marks: " + maths);
+        System.out.println("Statistics marks: " + statistics);
+        System.out.println("Computers marks: " + computers);
+        System.out.println("Total marks: " + total);
+        System.out.println("Average marks: " + average);
+    }
+}
+
