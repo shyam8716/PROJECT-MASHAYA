84c4a1e57835c1d1f23244d9fd9f8b04b3c47ce2
shyam <shyammegha493@gmail.com>
2025-09-02 12:08:49 +0530

    added PRIMITIVE DATA TYPES.java

---
+++ PRIMITIVE DATA TYPES.java
@@ -0,0 +1,19 @@
+import java.io.*;
+class Primitive_data{
+    public static void main(String args[]){
+        byte myByte=121;
+        short myShort=32767;
+        int myInt=123456789;
+        long myLong = 987654321L; 
+        float myFloat = 1.2468f; 
+        double myDouble=2.357901;
+        char myChar='A';
+        System.out.println(myByte);
+        System.out.println(myShort);
+        System.out.println(myInt);
+        System.out.println(myLong);
+        System.out.println(myFloat);
+        System.out.println(myDouble);
+        System.out.println(myChar);
+    }
+} 
