d44acd22fb7495c6df0f421a1ae60e4a36135515
shyam <shyammegha493@gmail.com>
2025-09-03 12:04:17 +0530

    added FLOAT.java

---
+++ FLOAT.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.Scanner; 
+class Float {
+    public static void main(String abc[]) {
+        Scanner sc = new Scanner(System.in);
+        float num;
+
+        System.out.println("Enter a number:");
+        num = sc.nextFloat();
+
+        System.out.println("Floating value is: " + num);
+    }
+}
