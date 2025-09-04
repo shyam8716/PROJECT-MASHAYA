748348677ee2c04e143b31f547adb610468dd148
shyam <shyammegha493@gmail.com>
2025-09-04 11:19:51 +0530

    added INTEGER.java

---
+++ INTEGER.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+class Integer{
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int num;
+
+        System.out.println("Enter a number:");
+        num = sc.nextInt();
+
+        System.out.println("The number is: " + num);
+    }
+}
