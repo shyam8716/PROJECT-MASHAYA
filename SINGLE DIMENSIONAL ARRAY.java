91f0221b4aafd37673fd628aafbf64c8899cfffa
shyam <shyammegha493@gmail.com>
2025-09-06 11:23:08 +0530

    added SINGLE DIMENSIONAL ARRAY.java

---
+++ SINGLE DIMENSIONAL ARRAY.java
@@ -0,0 +1,9 @@
+import java.io.*;
+class Single_dimensional_array{
+    public static void main(String args[]){
+        int marks []={10,20,30,40,50,60,70,80,90,100};
+        for(int i=0;i<5;i++){
+            System.out.println(marks[i]);
+        }
+    }
+}
